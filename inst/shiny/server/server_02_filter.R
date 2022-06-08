1d7f2e02aa5021f76a1f0c16e65b3d6c957b1d4d
Jacob Agerbo <genomicsisawesome@gmail.com>
2022-06-08 21:33:27 +0200

    .
    .

--- inst/shiny/server/server_02_filter.R
+++ inst/shiny/server/server_02_filter.R
@@ -127,7 +127,8 @@ filter_summary_bar_density <- function(MAE, samples_discard = NULL, filter_type,
   if (num_levels > 8 & num_levels/nrow(sam_table) >= 0.3 & 
       !is.character(unlist(sam_table[, cov]))) {
     fit <- density(unlist(sam_table[, cov]))
-    num.density <- plot_ly(x = fit$x, y = fit$y, type = "scatter", colors = "YlOrRd",
+    num.density <- plot_ly(x = fit$x, y = fit$y, type = "scatter", colors = "YlOrRd", marker = list(color = "rgb(219,134,7)"), 
+                           line = list(color = "rgb(77,74,70)")
                            mode = "lines", fill = "tozeroy") %>% layout(title = cov, 
                                                                         xaxis = list(title = cov), yaxis = list(title = "Density"))
     num.density$elementId <- NULL
@@ -135,7 +136,8 @@ filter_summary_bar_density <- function(MAE, samples_discard = NULL, filter_type,
   }
   else {
     cat.df = data.frame(table(sam_table[, cov]))
-    cat.bar <- plot_ly(x = cat.df$Var1, y = cat.df$Freq, 
+    cat.bar <- plot_ly(x = cat.df$Var1, y = cat.df$Freq, marker = list(color = "rgb(219,134,7)"), 
+                       line = list(color = "rgb(77,74,70)")
                        type = "bar", colors = "YlOrRd", showlegend = FALSE) %>% layout(title = cov, 
                                                                     xaxis = list(tickmode = "array", showticklabels = TRUE, 
                                                                                  categoryorder = "trace"), yaxis = list(title = "Frequency"))
